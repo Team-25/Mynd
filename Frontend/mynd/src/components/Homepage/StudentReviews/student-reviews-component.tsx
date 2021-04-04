f20bdb2c9ddba50a7393f72c3dd5548f07f3548d
Mitrandir-main <dk00504@surrey.ac.uk>
2021-04-04 15:23:58 +0100

    [add] functional dynamic components

--- Frontend/mynd/src/components/Homepage/StudentReviews/student-reviews-component.tsx
+++ Frontend/mynd/src/components/Homepage/StudentReviews/student-reviews-component.tsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { Avatar, Card, Typography } from '@material-ui/core';
+import { CardActionArea } from '@material-ui/core';
+import { CardActions } from '@material-ui/core';
+import { CardContent } from '@material-ui/core';
+import { CardMedia } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 
 interface IProps {
     image: string;
@@ -9,9 +15,19 @@ interface IProps {
 const StudentReviewComponent: React.FC<IProps> = (props: IProps) => {
 
     return (
-        <div>
-
-        </div>
+        <Card className="card_reviews_component">
+            <CardActionArea>
+                <Avatar alt="Remy Sharp" src={props.image} />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {props.name}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        {props.text}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
     )
 }
 
