0be98d0b79bb726c581a42cae4e6d66ad4f64a4e
Harvey <hm00929@surrey.ac.uk>
2021-04-04 22:35:00 +0100

    [add] Created DescriptionBlock

---
+++ Frontend/mynd/src/components/Homepage/DescriptionBlock/DescriptionBlock.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DescriptionBlock = () => {
+
+    return (
+        <div className="homepage-description">
+            
+        </div>
+    )
+}
+
+export default DescriptionBlock;
---
+++ Frontend/mynd/src/components/Homepage/DescriptionBlock/UserTestimonial.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserTestimonial = () => {
+
+    return (
+        <div className="user-testimonial">
+            
+        </div>
+    )
+}
+
+export default UserTestimonial;
