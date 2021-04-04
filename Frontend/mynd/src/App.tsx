c96b5eaaccb7318640785b08863db9d45afdec2b
Harvey <hm00929@surrey.ac.uk>
2021-04-04 15:45:04 +0100

    Added node-sass package, changed app.css to app.scss file

--- Frontend/mynd/src/App.tsx
+++ Frontend/mynd/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import "./App.css";
+import "./App.scss";
 import StudentReviewsPage from './components/Homepage/StudentReviews/student-reviews';
 
 function App() {
