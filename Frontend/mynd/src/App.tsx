d3aee1e8608d87fc9f4135e57fc4f2dd42d94c98
Harvey <hm00929@surrey.ac.uk>
2021-04-06 17:26:28 +0100

    [add] 404 Page with handling

--- Frontend/mynd/src/App.tsx
+++ Frontend/mynd/src/App.tsx
@@ -2,18 +2,22 @@ import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-d
 import "./App.scss";
 import Home from './pages/Home';
 import StudentReviewsPage from './components/Homepage/StudentReviews/student-reviews';
-import HeroSection from './components/Homepage/HeroSection/user-hero-section'
+import HeroSection from './components/Homepage/HeroSection/user-hero-section';
+
+import NotFound from './pages/404';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/home">
+          <HeroSection name="Jimmy"/>
         </Route>
-        <Route>
+        <Route exact path="/">
           {/* TODO If user logged in show Home-user, otherwise Home */}
           <Home />
         </Route>
+        <Route path='*' exact={true} component={NotFound} />
       </Switch>
     </Router>
   );
---
+++ Frontend/mynd/src/pages/404.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+    return (
+        <>
+            <h1 id="not-found">A 404 page. Better go back.</h1>
+        </>
+    );
+}
+
+export default NotFound;
