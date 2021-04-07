119aa8a0973accfba8f349f532bd3ae47b78890f
Harvey <hm00929@surrey.ac.uk>
2021-04-07 17:19:42 +0100

    [add] Moved question text to FAQ component

--- Frontend/mynd/src/components/Homepage/FAQs/FAQ-segment.tsx
+++ Frontend/mynd/src/components/Homepage/FAQs/FAQ-segment.tsx
@@ -2,8 +2,11 @@ import { Avatar } from '@material-ui/core';
 import React from 'react';
 
 
-interface IProps {
+/*==== TODO: NEED TO ADD IMG TO PROPS ====*/
 
+interface IProps {
+    question: string;
+    answer: string;
 }
 
 const FAQSegment = (props: IProps) => {
@@ -12,9 +15,9 @@ const FAQSegment = (props: IProps) => {
         <div className="FAQSegment-container">
             <div className="top-layer">
                 <Avatar className="FAQ-image" src="https://st4.depositphotos.com/27941032/30853/v/950/depositphotos_308538152-stock-illustration-target-minimal-thin-line-web.jpg" alt="test"/>
-                <h2>Incredible Question</h2>
+                <h2>{props.question}</h2>
             </div>
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
+            <p>{props.answer}</p>
         </div>
     )
 }
--- Frontend/mynd/src/components/Homepage/FAQs/FAQs.tsx
+++ Frontend/mynd/src/components/Homepage/FAQs/FAQs.tsx
@@ -8,12 +8,25 @@ interface IProps {
 
 const FAQs = (props: IProps) => {
 
+    const questions = [
+        {
+            "question": 'Question 1',
+            "answer": 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.'
+        },
+        {
+            "question": 'Question 2',
+            "answer": 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.'
+        },
+        {
+            "question": 'Question 3',
+            "answer": 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.'
+        }
+    ];
+
     return (
         <div className="faqs-container container">
             <h1>FAQs</h1>
-            <FAQSegment />
-            <FAQSegment />
-            <FAQSegment />
+            {questions.map((q) => <FAQSegment question={q.question} answer={q.answer} />)}
         </div>
     )
 }
