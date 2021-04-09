9fffbcf382437c4dba094c6196891a5b9cdcfba6
Alex Williams <aw01121@surrey.ac.uk>
2021-04-09 04:34:54 +0100

    [add] Added dark mode to calendar

--- Frontend/mynd/src/components/Calendar/calendar-event.tsx
+++ Frontend/mynd/src/components/Calendar/calendar-event.tsx
@@ -15,21 +15,21 @@ class CalendarEvent extends React.Component<IProps> {
 
     render() {
         return (
-            <Card className="calendar-event-card">
-            <CardActionArea>
-                <CardContent>
-                <Typography variant="h6" component="h2">
-                        {this.props.time}
-                    </Typography>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        {this.props.title}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        Speaking by: {this.props.speakers}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
+            <div className="calendar-event-card">
+                <CardActionArea>
+                    <CardContent>
+                    <Typography variant="h6" component="h2">
+                            {this.props.time}
+                        </Typography>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            {this.props.title}
+                        </Typography>
+                        <Typography variant="body2"component="p">
+                            Speaking by: {this.props.speakers}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </div>
         )
     }
 }
