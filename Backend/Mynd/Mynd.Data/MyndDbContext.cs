fcaa6d9672d41db9a918f8a25fd4c2fefa749bbf
Mitrandir-main <dk00504@surrey.ac.uk>
2021-04-10 11:47:24 +0100

    [add] entity framework implementation in main api

---
+++ Backend/Mynd/Mynd.Data/MyndDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+
+namespace Mynd.Data.Models
+{
+    public class MyndDbContext : DbContext
+    {
+    }
+}
