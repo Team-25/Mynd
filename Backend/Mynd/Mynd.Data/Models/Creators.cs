51cc6fe33c79ab5ef2142e53f808683ada49c5d8
Mitrandir-main <dk00504@surrey.ac.uk>
2021-04-10 16:40:18 +0100

    [update] model

--- Backend/Mynd/Mynd.Data/Models/Creators.cs
+++ Backend/Mynd/Mynd.Data/Models/Creators.cs
@@ -2,17 +2,24 @@
 
 namespace Mynd.Data.Models
 {
-    [Table("creators")]
+    [Table("the_creators")]
     public class Creator
     {
+        [Column("id")]
+        public long Id { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
         [Column("email")]
         public string Email { get; set; }
 
-        [Column("Name")]
-        public string Name { get; set; }
-
         [Column("role")]
         public string Role { get; set; }
+
+        [Column("big_pp?")]
+        public bool BigPP { get; set; }
+
 
     }
 }
