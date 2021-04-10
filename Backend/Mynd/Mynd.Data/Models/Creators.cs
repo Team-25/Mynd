21275f342bd8a6baf8b61ab20b3d3f8c73dec23c
Mitrandir-main <dk00504@surrey.ac.uk>
2021-04-10 13:04:05 +0100

    [add] models implementation

---
+++ Backend/Mynd/Mynd.Data/Models/Creators.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mynd.Data.Models
+{
+    [Table("creators")]
+    class Creators
+    {
+        [Column("email")]
+        public string Email { get; set; }
+
+        [Column("Name")]
+        public string Name { get; set; }
+
+        [Column("role")]
+        public string Role { get; set; }
+
+    }
+}
