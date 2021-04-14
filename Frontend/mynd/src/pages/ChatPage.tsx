7aac1bc7c78d2a54229adacc178206108a50218c
Alex Williams <aw01121@surrey.ac.uk>
2021-04-14 15:50:23 +0100

    [added] sidebar and routes

--- Frontend/mynd/src/pages/ChatPage.tsx
+++ Frontend/mynd/src/pages/ChatPage.tsx
@@ -2,22 +2,24 @@ import { Grid } from "@material-ui/core";
 import React from "react"
 import Chat from '../components/Chat/chat'
 import '../components/Chat/chat.scss'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 interface IProps {
 }
 
 const ChatPage = (props: IProps) => {
     return (
-        <div className="chat-page-container">
-            <Grid container spacing={2}>
-                <Grid item xs={2}>
-                    <div className="people">HELLO</div>
-                </Grid>
-                <Grid item xs={10}>
-                    <Chat />
-                </Grid>
-            </Grid>
-        </div>
+        <>
+            <Router>
+                <Switch>
+                    <Route path='/chat/:roomId'>
+                        <Chat />
+                    </Route>
+                    <Route path='/chat/'>
+                    </Route>
+                </Switch>
+            </Router>
+        </>
     )
 }
 
