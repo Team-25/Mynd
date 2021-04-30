6d6596e05b8a3f65948e13bf52253ef8f8c5e066
Mitrandir-main <dk00504@surrey.ac.uk>
2021-04-30 20:29:35 +0100

    [add] profile page functional implementation

--- Mynd/ClientApp/src/components/Profile/profile.tsx
+++ Mynd/ClientApp/src/components/Profile/profile.tsx
@@ -1,20 +1,22 @@
 import { Paper } from "@material-ui/core";
 import { Grid } from "@material-ui/core";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase";
 
 interface IProps {}
 
 function ProfilePage(props: IProps) {
   const [user] = useAuthState(auth);
   let photoURL: string = user?.photoURL || "";
-  let displayName: string = "Hi, " + (user?.displayName?.split(" ")[0] || "");
+  let displayName: string = user?.displayName?.split(" ")[0] || "";
 
   return (
     <div>
       <Grid container spacing={2}>
         <Grid item xs={4}>
           <Paper variant="outlined">
-            <img src="url" />
+            <img src={photoURL} />
+            <h2>{displayName}</h2>
           </Paper>
         </Grid>
       </Grid>
