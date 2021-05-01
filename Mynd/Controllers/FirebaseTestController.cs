2cddfcea9fd7602c2f8a53cd270455ca3e9af14c
Mitrandir-main <dk00504@surrey.ac.uk>
2021-05-01 21:56:39 +0100

    [add] firebase test controller skeleton

---
+++ Mynd/Controllers/FirebaseTestController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FireSharp.Config;
+using FireSharp.Response;
+using FireSharp.Interfaces;
+
+
+namespace Mynd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FirebaseTestController : ControllerBase
+    {
+
+
+        IFirebaseConfig form = new FirebaseConfig();
+
+        [HttpGet("creators")]
+        public async Task<List<Creator>> GetCreators(bool PPisBig)
+        {
+
+            return null;
+
+        }
+    }
+}
