68bc8926e90d0ede8218702f6b7e5abc1d4b3171
Alex Williams <aw01121@surrey.ac.uk>
2021-05-02 06:35:19 +0100

    [add] Added news CSS/responsiveness

--- Mynd/ClientApp/src/pages/NewsPage.tsx
+++ Mynd/ClientApp/src/pages/NewsPage.tsx
@@ -12,17 +12,12 @@ const NewsPage = (props: IProps) => {
             <div className="news-header">
                 <h1> News on Covid-19</h1>
             </div>
-            <Grid container spacing={2}>
-                <Grid item xs={3}>
-                    <TwitterContainer account="NHS" />
-                </Grid>
-                <Grid item xs={3}>                   
-                    <TwitterContainer account="WHO" />
-                </Grid>
-                <Grid item xs={6}>
-                <div style={{maxWidth:500, height: 300}}><iframe src="https://api-bridge.azurewebsites.net/conditions/?p=coronavirus-covid-19&aspect=name,overview_short,symptoms_short,symptoms_long,treatments_overview_short,other_treatments_long,self_care_long,prevention_short,causes_short&tab=3&uid=localhost:3000" title="NHS website - health a-z" style={{height: 500, width: 500}}></iframe></div>
-                </Grid>
-            </Grid>
+            <div className="news-grid">
+                        <TwitterContainer account="NHS" />
+                        <TwitterContainer account="WHO" />              
+                <div className="NHS-widget-container"><iframe src="https://api-bridge.azurewebsites.net/conditions/?p=coronavirus-covid-19&aspect=name,overview_short,symptoms_short,symptoms_long,treatments_overview_short,other_treatments_long,self_care_long,prevention_short,causes_short&tab=3&uid=localhost:3000" title="NHS website - health a-z" className="NHS-widget"></iframe></div>
+            </div>
+            
         </div>
     )
 }
