68f1b7f4deca6e2ef7c7aafe48191860ce72a192
Alex Williams <aw01121@surrey.ac.uk>
2021-05-02 06:03:51 +0100

    [add] added CSS and page grid

--- Mynd/ClientApp/src/pages/NewsPage.tsx
+++ Mynd/ClientApp/src/pages/NewsPage.tsx
@@ -1,9 +1,27 @@
+import { Grid } from "@material-ui/core";
+import '../components/News/news.scss'
 import React from "react"
+import TwitterContainer from '../components/News/twitter-timeline'
+
 interface IProps {
 }
+
 const NewsPage = (props: IProps) => {
     return (
         <div className="news-grid">
+            <div className="news-header">
+                <h1> News on Covid-19</h1>
+            </div>
+            <Grid container spacing={2}>
+                <Grid item xs={3}>
+                    <TwitterContainer account="NHS" />
+                </Grid>
+                <Grid item xs={3}>                   
+                    <TwitterContainer account="WHO" />
+                </Grid>
+                <Grid item xs={6}>
+                </Grid>
+            </Grid>
         </div>
     )
 }
